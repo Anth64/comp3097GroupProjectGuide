9859129dce287b7cc7268cde466ed75c096d40ce
101233471 <tosellonicole2001@gmail.com>
2022-04-19 17:10:59 -0400

    created Restaurants page

--- src/app/tabs/add/add.page.ts
+++ src/app/tabs/add/add.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-add',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add.page.scss'],
 })
 export class AddPage implements OnInit {
+  name = ''; address = ''; phoneNumber = ''; description = ''; 
 
-  constructor() { }
+  constructor(private route: Router) {}
 
   ngOnInit() {
+  }
+
+  addRestaurant() {
+
+  }
+
+  goBack() {
+    this.route.navigate(['/tabs/restaurants']);
   }
 
 }
