17c68881a4151f099d1881b1cb734023131aa2e2
101233471 <tosellonicole2001@gmail.com>
2022-04-19 16:40:52 -0400

    adding about page

--- src/app/about/about-routing.module.ts
+++ src/app/about/about-routing.module.ts
@@ -1,17 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { TabsPage } from './tabs.page';
+import { AboutPage } from './about.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: TabsPage
-  },
-  {
-    path: 'about',
-    loadChildren: () =>
-      import('../about/about.module').then((m) => m.AboutPageModule),
+    component: AboutPage
   }
 ];
 
@@ -19,4 +14,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class TabsPageRoutingModule {}
+export class AboutPageRoutingModule {}
