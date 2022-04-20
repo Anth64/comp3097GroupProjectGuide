78c7aa33cb376e6bf4aa7d6a37821372724ad5f7
anth64 <anth64@protonmail.com>
2022-04-20 15:27:34 -0400

    initialized edit page.

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -24,7 +24,11 @@ const routes: Routes = [
 			{
 				path: 'map',
 				loadChildren: () => import('./map/map.module').then( m => m.MapPageModule)
-			}
+			},
+			{
+				path: 'edit',
+				loadChildren: () => import('./edit/edit.module').then( m => m.EditPageModule)
+			},
 		]
   },
 ];
