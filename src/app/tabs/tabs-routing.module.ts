b90a37ac6256b8cd201578270145be9e3f99b09e
anth64 <anth64@protonmail.com>
2022-04-19 20:52:05 -0400

    fixed tab nav bar

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -6,21 +6,23 @@ import { TabsPage } from './tabs.page';
 const routes: Routes = [
   {
     path: '',
-    component: TabsPage
-  },
-  {
-    path: 'about',
-    loadChildren: () =>
-      import('../about/about.module').then((m) => m.AboutPageModule),
-  },  {
-    path: 'add',
-    loadChildren: () => import('./add/add.module').then( m => m.AddPageModule)
-  },
-  {
-    path: 'restaurants',
-    loadChildren: () => import('./restaurants/restaurants.module').then( m => m.RestaurantsPageModule)
+    component: TabsPage,
+		children: [
+			{
+				path: 'about',
+				loadChildren: () =>
+					import('../about/about.module').then((m) => m.AboutPageModule),
+			},
+			{
+				path: 'add',
+				loadChildren: () => import('./add/add.module').then( m => m.AddPageModule)
+			},
+			{
+				path: 'restaurants',
+				loadChildren: () => import('./restaurants/restaurants.module').then( m => m.RestaurantsPageModule)
+			}
+		]
   }
-
 ];
 
 @NgModule({
