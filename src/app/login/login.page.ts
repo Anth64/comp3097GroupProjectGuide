12291c9491e9c83998bd0490dff0398df09a765c
anth64 <anth64@protonmail.com>
2022-04-20 12:55:44 -0400

    login page works.

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -9,14 +10,19 @@ import { Router } from '@angular/router';
 export class LoginPage implements OnInit {
 	username = '';
 	password = '';
-
+	loginStatus = '';
   constructor(private route: Router) { }
 
   ngOnInit() {
   }
 
 	login() {
-
+		if(this.username.length === 0 || this.password.length === 0) {
+			this.loginStatus = 'No username or password entered!';
+			return;
+		}
+		this.loginStatus = '';
+		this.route.navigate(['/tabs/restaurants']);
 	}
 
 	register() {
